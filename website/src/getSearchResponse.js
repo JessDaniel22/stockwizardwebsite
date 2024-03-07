d5f7a400e2afb10ca8cf474b4d29db63def9c300
James <jmsyoung94@outlook.com>
2024-03-07 15:07:27 +0000

    Search bar stuff

--- website/src/getSearchResponse.js
+++ website/src/getSearchResponse.js
@@ -1,4 +1,4 @@
-async function getSearchResponse() {
+async function getSearchResponse(inputText) {
     const url = 'wss://cs261se.containers.uwcs.co.uk';
     const details = {"type": "SEARCH_REQUEST", "data": {
       "text" : inputText
